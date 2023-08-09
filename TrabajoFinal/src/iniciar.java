a75ee4b4440f2198031f342bc53209349de03b0c
Reyshellt <132863860+Reyshellt@users.noreply.github.com>
2023-08-09 08:24:30 -0400

    Principios de ventana

--- TrabajoFinal/src/iniciar.java
+++ TrabajoFinal/src/iniciar.java
@@ -3,6 +3,10 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import javax.swing.JInternalFrame;
+import javax.swing.JSplitPane;
 
 public class iniciar extends JFrame {
 
@@ -29,11 +33,29 @@ public class iniciar extends JFrame {
 	 */
 	public iniciar() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 763, 467);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel);
+		panel.setLayout(new BorderLayout(0, 0));
+		
+		JSplitPane splitPane = new JSplitPane();
+		splitPane.setEnabled(false);
+		splitPane.setForeground(new Color(242, 234, 225));
+		splitPane.setResizeWeight(0.2);
+		panel.add(splitPane, BorderLayout.CENTER);
+		
+		JPanel sidebar = new JPanel();
+		sidebar.setBackground(new Color(242, 234, 225));
+		splitPane.setLeftComponent(sidebar);
+		
+		JPanel panel_2 = new JPanel();
+		splitPane.setRightComponent(panel_2);
 	}
 
 }
--- TrabajoFinal/src/ventana.java
+++ TrabajoFinal/src/ventana.java
@@ -3,7 +3,9 @@ public class ventana {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		iniciar ventana_princila = new iniciar();
+		ventana_princila.setVisible(true);
+		
 	}
 
 }
