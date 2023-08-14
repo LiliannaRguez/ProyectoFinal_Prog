80abc2f0907b06943d7eef1c0e0a779aa9211918
Reyshellt <132863860+Reyshellt@users.noreply.github.com>
2023-08-13 22:44:26 -0400

    iniciando

--- TrabajoFinal/src/ventana.java
+++ TrabajoFinal/src/ventana.java
@@ -7,9 +7,14 @@ public class ventana {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		iniciar ventana_principal = new iniciar();
+		
+		/*iniciar ventana_principal = new iniciar();
 		 ventana_principal.setExtendedState(JFrame.MAXIMIZED_BOTH);
-		 ventana_principal.setVisible(true);
+		 ventana_principal.setVisible(true);*/
+		 
+		 LoginApp ventana_LoginApp = new LoginApp();
+		 ventana_LoginApp.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		 ventana_LoginApp.setVisible(true);
 		
 		 /*mysqlConexion objetoconexion = new mysqlConexion();
 			objetoconexion.estableceConexion();*/
